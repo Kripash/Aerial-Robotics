4a6737c1c2f97abb9777cf269268fd27650ba36c
Eric Duong <eduong@nevada.unr.edu>
2019-11-02 16:56:25 -0700

    error with detection, syntax

--- apriltag_detect/include/TagDetector.h
+++ apriltag_detect/include/TagDetector.h
@@ -4,6 +4,11 @@
 #include <tag16h5.h>
 #include <ros/console.h>
 #include <sensor_msgs/Image.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <image_transport/image_transport.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 using std::string;
 
@@ -11,9 +16,10 @@ class TagDetector{
 private:
   apriltag_detector_t *td_;
   apriltag_family_t *tf_;
+  ros::NodeHandle nh_;
   ros::Subscriber sub_;
 
-  void detectTag(const sensor_msgs::ImagePtr& image);
+  void detectTag(const sensor_msgs::ImageConstPtr& image);
 
 public:
   TagDetector(int argc, char **argv, const string& node_name);
--- apriltag_detect/src/TagDetector.cpp
+++ apriltag_detect/src/TagDetector.cpp
@@ -15,14 +15,29 @@ TagDetector::~TagDetector(){
 
 void TagDetector::init(int argc, char **argv, const string& node_name){
   ros::init(argc, argv, node_name);
-  ros::NodeHandle n;
-  sub_ = n.subscribe("ptgrey_node/cam/image_raw",1, &TagDetector::detectTag, this);
+  sub_ = nh_.subscribe("ptgrey_node/cam/image_raw",1, &TagDetector::detectTag, this);
 }
 
-void TagDetector::detectTag(const sensor_msgs::ImagePtr& image){
+void TagDetector::detectTag(const sensor_msgs::ImageConstPtr& image){
   zarray_t *detections = nullptr;
+  cv_bridge::CvImagePtr cv_ptr;
+  try
+  {
+    cv_ptr = cv_bridge::toCvCopy(image, sensor_msgs::image_encodings::MONO8);
+  }
+  catch (cv_bridge::Exception& e)
+  {
+    ROS_ERROR("cv_bridge exception: %s", e.what());
+    return;
+  }
+  cv::Mat gray_image = cv_ptr->image;
 
-  image_u8_t apriltag_image = {image->};
+  image_u8_t apriltag_image = {
+    .width = gray_image.cols,
+    .height = gray_image.rows,
+    .stride = gray_image.cols,
+    .buf = gray_image.data
+  };
 
   detections = apriltag_detector_detect(td_, apriltag_image);
   for (int i = 0; i < zarray_size(detections); i++) {
