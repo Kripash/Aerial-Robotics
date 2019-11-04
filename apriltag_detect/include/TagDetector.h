705ade0b239c3567fe0de417f3d044cfae95d0b3
Eric Duong <eduong@nevada.unr.edu>
2019-11-04 11:43:27 -0800

    pose found, do something with it

--- apriltag_detect/include/TagDetector.h
+++ apriltag_detect/include/TagDetector.h
@@ -1,12 +1,15 @@
 #include <ros/ros.h>
 #include <string>
+#include <memory>
 #include <apriltag.h>
+#include <apriltag_pose.h>
 #include <tag16h5.h>
 #include <ros/console.h>
 #include <sensor_msgs/Image.h>
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
 #include <image_transport/image_transport.h>
+#include "image_geometry/pinhole_camera_model.h"
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
@@ -18,9 +21,13 @@ private:
   apriltag_family_t *tf_;
   ros::NodeHandle nh_;
   ros::Subscriber sub_;
+  image_transport::ImageTransport it_;
+  image_transport::CameraSubscriber camera_image_subscriber_;
   zarray_t *detected_tags_;
 
-  void detectTag(const sensor_msgs::ImageConstPtr& image);
+  void detectTag(
+    const sensor_msgs::ImageConstPtr& image,
+    const sensor_msgs::CameraInfoConstPtr& camera_info);
   static int idComparison(const void* first, const void* second);
   void removeDuplicates();
 
--- apriltag_detect/src/TagDetector.cpp
+++ apriltag_detect/src/TagDetector.cpp
@@ -4,9 +4,9 @@ TagDetector::TagDetector():
   td_{apriltag_detector_create()},
   tf_{tag16h5_create()},
   nh_{},
+  it_{nh_},
   detected_tags_{nullptr}
 {
-  apriltag_detector_add_family(td_, tf_);
   init();
 }
 
@@ -16,10 +16,16 @@ TagDetector::~TagDetector(){
 }
 
 void TagDetector::init(){
-  sub_ = nh_.subscribe("ptgrey_node/cam/image_raw",1, &TagDetector::detectTag, this);
+  apriltag_detector_add_family(td_, tf_);
+  camera_image_subscriber_ = it_.subscribeCamera(
+    "image_rect", 1, &TagDetector::detectTag, this);
+  // sub_ = nh_.subscribe("ptgrey_node/cam",1, &TagDetector::detectTag, this);
 }
 
-void TagDetector::detectTag(const sensor_msgs::ImageConstPtr& image){
+void TagDetector::detectTag(
+  const sensor_msgs::ImageConstPtr& image,
+  const sensor_msgs::CameraInfoConstPtr& camera_info)
+{
   cv_bridge::CvImagePtr cv_ptr;
   try {
     cv_ptr = cv_bridge::toCvCopy(image, sensor_msgs::image_encodings::MONO8);
@@ -28,6 +34,12 @@ void TagDetector::detectTag(const sensor_msgs::ImageConstPtr& image){
     return;
   }
   cv::Mat gray_image = cv_ptr->image;
+  image_geometry::PinholeCameraModel camera_model;
+  camera_model.fromCameraInfo(camera_info);
+  double fx = camera_model.fx();
+  double fy = camera_model.fy();
+  double cx = camera_model.cx();
+  double cy = camera_model.cy();
 
   image_u8_t apriltag_image = {
     .width = gray_image.cols,
@@ -36,20 +48,29 @@ void TagDetector::detectTag(const sensor_msgs::ImageConstPtr& image){
     .buf = gray_image.data
   };
 
+  if (detected_tags_){
+    apriltag_detections_destroy(detected_tags_);
+    detected_tags_ = nullptr;
+  }
   detected_tags_ = apriltag_detector_detect(td_, &apriltag_image);
-  // for (int i = 0; i < zarray_size(detections); i++) {
-  //   apriltag_detection_t *det;
-  //   zarray_get(detections, i, &det);
-  //
-  //   ROS_INFO_STREAM("FOUND! " << i);
-  // }
   removeDuplicates();
-  ROS_INFO_STREAM("FOUND! " << zarray_size(detected_tags_));
+
   for (int i = 0; i < zarray_size(detected_tags_); i++) {
     apriltag_detection_t *det;
     zarray_get(detected_tags_, i, &det);
-
-    ROS_INFO_STREAM("ID: " << det->id);
+    if (det->id == 4){
+      // ROS_INFO_STREAM("FOUND!");
+      apriltag_detection_info_t info;
+      apriltag_pose_t pose;
+      info.det = det;
+      info.tagsize = 0.2286;
+      info.fx = fx;
+      info.fy = fy;
+      info.cx = cx;
+      info.cy = cy;
+      double err = estimate_tag_pose(&info, &pose);
+      // TODO: do something with pose.
+    }
   }
 }
 
